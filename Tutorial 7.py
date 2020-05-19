22e28f8f6fe444af7e0cce0e4cee5571ea1c0a05
Yash Gupta <62286270+Yash25gupta@users.noreply.github.com>
2020-05-19 16:44:08 +0530

    added comments

--- Tutorial 7.py
+++ Tutorial 7.py
@@ -3,6 +3,9 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 
+# this is a comment
+# comment 2
+
 
 class Touch(Widget):
     btn = ObjectProperty(None)
